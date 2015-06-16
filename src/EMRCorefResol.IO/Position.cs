0ff241001cafd788eb88b58f739e2be77180869e
Nguyen Duy Hung <hungnd1475@outlook.com>
2015-06-16 22:47:49 +0700

    initial commit for c# solution

---
+++ src/EMRCorefResol.IO/Position.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMRCorefResol.Shared
+{
+	public struct Position : IComparable, IComparable<Position>
+	{
+		public int Line { get; private set; }
+		public int Column { get; private set; }
+
+		public Position(int line, int column)
+			: this()
+		{
+			this.Line = line;
+			this.Column = column;
+		}
+
+		public int CompareTo(Position other)
+		{
+			if (this.Line == other.Line)
+			{
+				return this.Column - other.Column;
+			}
+			else
+			{
+				return this.Line - other.Line;
+			}
+		}
+
+		public int CompareTo(object obj)
+		{
+			var position = obj as Position?;
+			if (position != null)
+				return CompareTo(position.Value);
+			throw new ArgumentNullException();
+		}
+	}
+}
