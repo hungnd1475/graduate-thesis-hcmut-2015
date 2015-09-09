127fbf6dae8ce9981a3dcaf53b354b31e9eedeeb
Tuấn Vương <vanhtuan0409@gmail.com>
2015-09-09 10:27:40 +0700

    implement NameMatch / I / You / We feature

---
+++ projects/emr-coreference-resolution/EMRCorefResol.English/Features/PersonPair/IInformationFeature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCMUT.EMRCorefResol.English.Features
+{
+    class IInformationFeature : Feature
+    {
+        public IInformationFeature(PersonPair instance)
+            : base("I-Information")
+        {
+            Value = (string.Equals(instance.Anaphora.Lexicon.ToLower(), "i") &&
+                    string.Equals(instance.Antecedent.Lexicon.ToLower(), "i")) ?
+                1.0 : 0.0;
+        }
+    }
+}
