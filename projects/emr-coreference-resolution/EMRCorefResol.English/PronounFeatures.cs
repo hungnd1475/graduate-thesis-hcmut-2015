951f85ea83e62a9a9189020ed41ef8e06d541118
Tuấn Vương <vanhtuan0409@gmail.com>
2015-09-14 20:31:56 +0700

    add semantic feature "and" "in addition to" "as well as"

--- projects/emr-coreference-resolution/EMRCorefResol.English/PronounFeatures.cs
+++ projects/emr-coreference-resolution/EMRCorefResol.English/PronounFeatures.cs
@@ -11,12 +11,13 @@ namespace HCMUT.EMRCorefResol.English
     class PronounFeatures : FeatureVector
     {
         public PronounFeatures(PronounInstance instance, EMR emr, CorefChainCollection groundTruth, double classValue)
-            : base(size: 4, classValue: classValue)
+            : base(size: 5, classValue: classValue)
         {
             this[0] = new FirstPreviousMentionTypeFeature(instance, emr);
             this[1] = new SecondPreviousMentionTypeFeature(instance, emr);
             this[2] = new FirstNextMentionTypeFeature(instance, emr);
             this[3] = new PartOfSpeechFeature(instance);
+            this[4] = new SemanticFeature(instance, emr);
         }
     }
 }
