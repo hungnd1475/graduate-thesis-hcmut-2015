d1638c003c8a5f34b8ddf61db0e34cceff128b66
Tuấn Vương <vanhtuan0409@gmail.com>
2015-09-14 09:27:23 +0700

    add wordnet singleton

---
+++ projects/emr-coreference-resolution/EMRCorefResol.English/Utilities/WordnetHelper.cs
@@ -0,0 +1,21 @@
+﻿using LAIR.ResourceAPIs.WordNet;
+
+namespace HCMUT.EMRCorefResol.English
+{
+    public sealed class WordnetHelper
+    {
+        private static readonly WordNetEngine wEngine = new WordNetEngine(@"../../libs/wordnet/wordnetdb", true);
+
+        static WordnetHelper()
+        {
+        }
+
+        public static WordNetEngine Instance
+        {
+            get
+            {
+                return wEngine;
+            }
+        }
+    }
+}
