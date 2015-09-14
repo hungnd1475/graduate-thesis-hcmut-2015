0f78317a3c4439b0582365e909c5f1e9b52ac360
Tuấn Vương <vanhtuan0409@gmail.com>
2015-09-14 14:02:09 +0700

    add SharpNLP help for language processing

---
+++ projects/emr-coreference-resolution/EMRCorefResol.English/Utilities/SharpNLPHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenNLP.Tools.PosTagger;
+using OpenNLP.Tools.Tokenize;
+
+namespace HCMUT.EMRCorefResol.English.Utilities
+{
+    public sealed class SharpNLPHelper
+    {
+        private static string modelsURL = @"../../libs/sharpNLP/Models/";
+
+        private static readonly EnglishMaximumEntropyPosTagger mPostTagger = new EnglishMaximumEntropyPosTagger(modelsURL + "EnglishPOS.nbin", modelsURL + "Parser/tagdict");
+
+        private static readonly EnglishMaximumEntropyTokenizer mTokenizer = new EnglishMaximumEntropyTokenizer(modelsURL + "EnglishTok.nbin");
+
+        public static EnglishMaximumEntropyPosTagger TaggerInstance
+        {
+            get
+            {
+                return mPostTagger;
+            }
+        }
+
+        public static EnglishMaximumEntropyTokenizer TokenizerInstance
+        {
+            get
+            {
+                return mTokenizer;
+            }
+        }
+    }
+}
