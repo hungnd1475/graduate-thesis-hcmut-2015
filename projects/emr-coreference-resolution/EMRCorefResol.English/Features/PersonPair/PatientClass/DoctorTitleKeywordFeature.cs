3bc861781d615696aeac429f41be3fe7669fdf1a
Tuấn Vương <vanhtuan0409@gmail.com>
2015-09-16 13:16:27 +0700

    add the rest of keyword feature for patient class

---
+++ projects/emr-coreference-resolution/EMRCorefResol.English/Features/PersonPair/PatientClass/DoctorTitleKeywordFeature.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HCMUT.EMRCorefResol.English.Features.PersonPair.PatientClass
+{
+    class DoctorTitleKeywordFeature : Feature
+    {
+        public DoctorTitleKeywordFeature(PersonInstance instance)
+            : base("DoctorTitle-Keyword")
+        {
+            string[] stopWords = { "anesthesiologist", "orthodontist", "dentist" };
+            foreach (string stopWord in stopWords)
+            {
+                if (checkContain(instance.Concept.Lexicon.ToLower(), stopWord))
+                {
+                    Value = 1.0;
+                    return;
+                }
+            }
+            Value = 0.0;
+        }
+
+        private bool checkContain(string s1, string s2)
+        {
+            return Regex.IsMatch(s1, string.Format(@"\b{0}\b", Regex.Escape(s2)));
+        }
+    }
+}
---
+++ projects/emr-coreference-resolution/EMRCorefResol.English/Features/PersonPair/PatientClass/GeneralDoctorKeywordFeature.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HCMUT.EMRCorefResol.English.Features.PersonPair.PatientClass
+{
+    class GeneralDoctorKeywordFeature : Feature
+    {
+        public GeneralDoctorKeywordFeature(PersonInstance instance)
+            : base("GeneralDoctor-Keyword")
+        {
+            string[] stopWords = { "doctor", "pcp", "author", "dict", "attend", "provider" };
+            foreach (string stopWord in stopWords)
+            {
+                if (checkContain(instance.Concept.Lexicon.ToLower(), stopWord))
+                {
+                    Value = 1.0;
+                    return;
+                }
+            }
+            Value = 0.0;
+        }
+
+        private bool checkContain(string s1, string s2)
+        {
+            return Regex.IsMatch(s1, string.Format(@"\b{0}\b", Regex.Escape(s2)));
+        }
+    }
+}
