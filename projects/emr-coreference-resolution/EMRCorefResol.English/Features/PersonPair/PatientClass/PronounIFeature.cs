a24bff0b794af31fb34ec29bf3d1f77ce7f80aa3
Tuấn Vương <vanhtuan0409@gmail.com>
2015-09-16 12:49:08 +0700

    add patient class grammatical feature (pronoun we/i/you/they)

---
+++ projects/emr-coreference-resolution/EMRCorefResol.English/Features/PersonPair/PatientClass/PronounIFeature.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HCMUT.EMRCorefResol.English.Features
+{
+    class PronounIFeature : Feature
+    {
+        public PronounIFeature(PersonInstance instance)
+            : base("Prounoun-I")
+        {
+            Value = (checkContain(instance.Concept.Lexicon.ToLower(), "i") ||
+                checkContain(instance.Concept.Lexicon.ToLower(), "me") ||
+                checkContain(instance.Concept.Lexicon.ToLower(), "my") ||
+                checkContain(instance.Concept.Lexicon.ToLower(), "myself")) ?
+                1.0 : 0.0;
+        }
+
+        private bool checkContain(string s1, string s2)
+        {
+            return Regex.IsMatch(s1, string.Format(@"\b{0}\b", Regex.Escape(s2)));
+        }
+    }
+}
---
+++ projects/emr-coreference-resolution/EMRCorefResol.English/Features/PersonPair/PatientClass/PronounYouFeature.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HCMUT.EMRCorefResol.English.Features
+{
+    class PronounYouFeature : Feature
+    {
+        public PronounYouFeature(PersonInstance instance)
+            : base("Pronoun-You")
+        {
+            Value = (checkContain(instance.Concept.Lexicon.ToLower(), "you") ||
+                checkContain(instance.Concept.Lexicon.ToLower(), "your") ||
+                checkContain(instance.Concept.Lexicon.ToLower(), "yourself")) ?
+                1.0 : 0.0;
+        }
+
+        private bool checkContain(string s1, string s2)
+        {
+            return Regex.IsMatch(s1, string.Format(@"\b{0}\b", Regex.Escape(s2)));
+        }
+    }
+}
