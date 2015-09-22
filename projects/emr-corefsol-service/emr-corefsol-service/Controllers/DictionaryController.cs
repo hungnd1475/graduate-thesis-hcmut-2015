52ca9b1ea29cf63619df0f0d5e3ff20a417b1595
Tuấn Vương <vanhtuan0409@gmail.com>
2015-09-22 15:40:48 +0700

    add Web Service project

---
+++ projects/emr-corefsol-service/emr-corefsol-service/Controllers/DictionaryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+
+using emr_corefsol_service.Response;
+using emr_corefsol_service.Libs;
+
+namespace emr_corefsol_service.Controllers
+{
+    public class DictionaryController : ApiController
+    {
+        /// <summary>
+        /// GET WordNet Synset of a word
+        /// </summary>
+        /// <param name="term">Lookup term</param>
+        /// <returns></returns>
+        [ActionName("Synsets")]
+        public CustomResponse GetSynsets(string term)
+        {
+            if(term.Length > 0)
+            {
+                var defs = WordNetHelper.getSynSets(term);
+                return new CustomResponse(true, defs, null);
+            } else
+            {
+                return new CustomResponse(false, null, "No term submitted");
+            }
+        }
+    }
+}
---
+++ projects/emr-corefsol-service/emr-corefsol-service/Libs/WordNetHelper.cs
@@ -0,0 +1,36 @@
+﻿using LAIR.ResourceAPIs.WordNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace emr_corefsol_service.Libs
+{
+    public class WordNetHelper
+    {
+        private static readonly string dbUrl = HostingEnvironment.MapPath(@"~\app_data\libs\wordnet\wordnetdb");
+        private static readonly WordNetEngine wEngine = new WordNetEngine(dbUrl, true);
+        private static readonly WordNetSimilarityModel wSimilarity = new WordNetSimilarityModel(wEngine);
+
+        static WordNetHelper()
+        {
+
+        }
+
+        public static Models.Definition[] getSynSets(string term)
+        {
+            var t = wEngine.GetSynSets(term);
+            Models.Definition[] res = new Models.Definition[t.Count];
+
+            int i = 0;
+            foreach (SynSet s in t)
+            {
+                res[i] = new Models.Definition(s);
+                i++;
+            }
+
+            return res;
+        }
+    }
+}
