beb81a640fb29c3df8b438ad5d8d2b5163f6cfbd
Tuấn Vương <vanhtuan0409@gmail.com>
2015-09-23 09:41:25 +0700

    rename models folder

---
+++ projects/emr-coreference-resolution/EMRCorefResol.Core/Service/ServiceModels/Definition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCMUT.EMRCorefResol.Service
+{
+    public class Definition
+    {
+        public WordType pos;
+        public string Gloss;
+        public string[] Words;
+        public Definition(WordType p, string g, string[] w)
+        {
+            pos = p;
+            Gloss = g;
+            Words = w;
+        }
+        public override string ToString()
+        {
+            return "(" + pos + "): " + Gloss;
+        }
+    }
+}
