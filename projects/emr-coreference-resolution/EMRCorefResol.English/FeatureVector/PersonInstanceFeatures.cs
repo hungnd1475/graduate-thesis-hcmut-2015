112c9ad702e5724e608908587c84cee92a4c4ec7
Tuấn Vương <vanhtuan0409@gmail.com>
2015-09-25 16:40:09 +0700

    add name feature for person instance (need to change remove stopword function)

--- projects/emr-coreference-resolution/EMRCorefResol.English/FeatureVector/PersonInstanceFeatures.cs
+++ projects/emr-coreference-resolution/EMRCorefResol.English/FeatureVector/PersonInstanceFeatures.cs
@@ -11,7 +11,7 @@ namespace HCMUT.EMRCorefResol.English
     class PersonInstanceFeatures : FeatureVector
     {
         public PersonInstanceFeatures(PersonInstance instance, EMR emr, double classValue)
-            : base(size: 6, classValue: classValue)
+            : base(size: 7, classValue: classValue)
         {
             this[0] = new PronounIFeature(instance);
             this[1] = new PronounYouFeature(instance);
@@ -19,6 +19,7 @@ namespace HCMUT.EMRCorefResol.English
             this[3] = new PronounTheyFeature(instance);
             this[4] = new PatientKeywordFeature(instance);
             this[5] = new DoctorKeywordFeature(instance);
+            this[6] = new NameFeature(instance, emr);
         }
     }
 }
---
+++ projects/emr-coreference-resolution/EMRCorefResol.English/Features/PersonInstance/NameFeature.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCMUT.EMRCorefResol.English.Features
+{
+    class NameFeature : Feature
+    {
+        public NameFeature(PersonInstance instance, EMR emr)
+            :base("Name-Feature", 2, 0)
+        {
+            var line = EMRExtensions.GetLine(emr, instance.Concept.Begin.Line).Split(' ');
+            var rawNameArr = line.Skip(instance.Concept.Begin.WordIndex)
+                .Take(instance.Concept.End.WordIndex - instance.Concept.Begin.WordIndex + 1)
+                .ToArray();
+
+            var nameArr = removeStopWords(string.Join(" ", rawNameArr)).Split(' ');
+
+            if(Char.IsUpper(nameArr.First()[0]) && Char.IsUpper(nameArr.Last()[0]))
+            {
+                SetCategoricalValue(1);
+            }
+
+            int x = 1 + 1;
+        }
+
+        private string removeStopWords(string raw)
+        {
+            string[] stopwords = { "mr", "mr.", "mrs", "mrs.", "ms", "ms.", "dr", "dr.",
+                                   "md", "m.d", "m.d.", "md.",
+                                   "phd", "phd.", "prof", "prof."
+                                };
+
+            string[] nameArr = raw.ToLower().Split(' ');
+
+            List<string> tmp = new List<string>();
+            for (int i = 0; i < nameArr.Length; i++)
+            {
+                if (!stopwords.Contains(nameArr[i]))
+                {
+                    tmp.Add(nameArr[i].Replace("'", "").Replace("\"", "").Replace(":", ""));
+                }
+            }
+            return String.Join(" ", tmp);
+        }
+    }
+}
