4e64becd33752f3d27244f589d27f5d108af36b0
Nguyen Duy Hung <hungnd1475@outlook.com>
2015-10-12 16:28:36 +0700

    remove English Properties Keywords

--- projects/emr-coreference-resolution/EMRCorefResol.English/Features/PersonPair/DoctorGeneralMatch.cs
+++ projects/emr-coreference-resolution/EMRCorefResol.English/Features/PersonPair/DoctorGeneralMatch.cs
@@ -1,5 +1,4 @@
-﻿using HCMUT.EMRCorefResol.English.Properties;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,22 +12,6 @@ namespace HCMUT.EMRCorefResol.English.Features
         public DoctorGeneralMatch(PersonPair instance)
             : base("Doctor-General-Match", 2, 0)
         {
-            /*var drGenerals = Settings.Default.GeneralDoctors;
-            var anteLex = instance.Antecedent.Lexicon.ToLower();
-            var anaLex = instance.Anaphora.Lexicon.ToLower();
-            string kw = null;
-
-            foreach (var dg in drGenerals)
-            {
-                if (anteLex.Contains(dg))
-                {
-                    kw = dg;
-                    break;
-                }
-            }
-
-            SetCategoricalValue((kw != null && anaLex.Contains(kw)) ? 1 : 0);*/
-
             var searcher = KeywordService.Instance.GENERAL_DOCTOR;
             var kws = searcher.SearchKeywords(instance.Anaphora.Lexicon, KWSearchOptions.WholeWord | KWSearchOptions.IgnoreCase);
             if(kws == null || kws.Length < 1)
--- projects/emr-coreference-resolution/EMRCorefResol.English/Features/PersonPair/DoctorTitleMatchFeature.cs
+++ projects/emr-coreference-resolution/EMRCorefResol.English/Features/PersonPair/DoctorTitleMatchFeature.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using HCMUT.EMRCorefResol.English.Properties;
 
 namespace HCMUT.EMRCorefResol.English.Features
 {
