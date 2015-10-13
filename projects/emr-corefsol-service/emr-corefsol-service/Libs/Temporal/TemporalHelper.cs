b86409b6b0e8bf0f38181bba796c5c717b7898cf
Tuấn Vương <vanhtuan0409@gmail.com>
2015-10-13 22:20:16 +0700

    update explicit date by regex

--- projects/emr-corefsol-service/emr-corefsol-service/Libs/Temporal/TemporalHelper.cs
+++ projects/emr-corefsol-service/emr-corefsol-service/Libs/Temporal/TemporalHelper.cs
@@ -11,20 +11,40 @@ namespace emr_corefsol_service.Libs
     public class TemporalHelper
     {
         private PythonExcuter _excuter = null;
-        private List<Regex> _list = null;
+        private List<Regex> _inferred = null;
+        private List<Regex> _explicit = null;
+        private string year_re = @"[12][0-9][0-9][0-9]";
+        private string month_re = @"[01][0-9]";
+        private string day_re = @"[0123][0-9]";
+        private string delimiter = @"[\\/-]";
+
         public TemporalHelper(PythonExcuter e)
         {
             _excuter = e;
             var normaliser = HostingEnvironment.MapPath(@"~\app_data\tools\Clinical-norma\normaliser.py");
             _excuter.SetScriptFile(normaliser);
 
-            _list = new List<Regex>();
-            _list.Add(new Regex(@"(?:the |her |his |their )?(?:post-|post|day)? ?(?:pod|operative|op|hospital|hsp|day|hd)(?:ly)? ?(?:day |night |afternoon )? ?(?:number|num\.?|#)? ?([0-9][0-9]*)"));
-            _list.Add(new Regex(@"(?:the |her |his |their )?([0-9][0-9]*)(?:st|nd|rd|th)? (?:post-|post|day)? ?(?:pod|operative|op|hospital|hsp|day|hd)(?:ly)? (?:day|night|afternoon)?"));
-            _list.Add(new Regex(@"(?:the)? ?([0-9][0-9]?)(?:st|nd|rd|th)? ?(-|to|or) ?([0-9][0-9]?)(?:st|nd|rd|th)? (?:post-|post|day)? ?(?:pod|operative|op|hospital|hsp|day|hd)(?:ly)? ?(?:days?|nights?|afternoons?)?"));
-            _list.Add(new Regex(@"(?:the |her |his |their )?day (?:of )?(?:the )?(discharge|admission|transfer|evaluation)"));
-            _list.Add(new Regex(@"(?:the|her|his|their)? ?day of life ?#? ?([0-9][0-9]*)"));
-            _list.Add(new Regex(@"(?:early)? ?(?:post)? ?(?:-)? ?(operative|extubation) ?(?:course)"));
+            _inferred = new List<Regex>();
+            _inferred.Add(new Regex(@"(?:the |her |his |their )?(?:post-|post|day)? ?(?:pod|operative|op|hospital|hsp|day|hd)(?:ly)? ?(?:day |night |afternoon )? ?(?:number|num\.?|#)? ?([0-9][0-9]*)"));
+            _inferred.Add(new Regex(@"(?:the |her |his |their )?([0-9][0-9]*)(?:st|nd|rd|th)? (?:post-|post|day)? ?(?:pod|operative|op|hospital|hsp|day|hd)(?:ly)? (?:day|night|afternoon)?"));
+            _inferred.Add(new Regex(@"(?:the)? ?([0-9][0-9]?)(?:st|nd|rd|th)? ?(-|to|or) ?([0-9][0-9]?)(?:st|nd|rd|th)? (?:post-|post|day)? ?(?:pod|operative|op|hospital|hsp|day|hd)(?:ly)? ?(?:days?|nights?|afternoons?)?"));
+            _inferred.Add(new Regex(@"(?:the |her |his |their )?day (?:of )?(?:the )?(discharge|admission|transfer|evaluation)"));
+            _inferred.Add(new Regex(@"(?:the|her|his|their)? ?day of life ?#? ?([0-9][0-9]*)"));
+            _inferred.Add(new Regex(@"(?:early)? ?(?:post)? ?(?:-)? ?(operative|extubation) ?(?:course)"));
+
+            _explicit = new List<Regex>();
+            //yyyymmdd
+            _explicit.Add(new Regex(year_re + month_re + day_re));
+            //mm/dd/yyyy
+            _explicit.Add(new Regex(month_re + delimiter +  day_re + delimiter + year_re));
+            //yyyy/mm/dd
+            _explicit.Add(new Regex(year_re + delimiter + month_re + delimiter + day_re));
+            //dd/mm/yyyy
+            _explicit.Add(new Regex(day_re + delimiter + month_re + delimiter + year_re));
+            //dd/mm/yy
+            _explicit.Add(new Regex(day_re + delimiter + month_re + delimiter + @"[0-9][0-9]"));
+            //mm/dd/yy
+            _explicit.Add(new Regex(month_re + delimiter + day_re + delimiter + @"[0-9][0-9]"));
         }
 
         public string GetTemporalValue(string emrPath, string line)
@@ -32,21 +52,14 @@ namespace emr_corefsol_service.Libs
             try
             {
                 //Get explicit date
-                _excuter.Excute(new string[] { "normaliser.py", emrPath, line });
-
-                dynamic result = _excuter.GetVariable("res");
-                if (result == null)
+                var expl = GetExplicitDate(line);
+                if(expl != null)
                 {
-                    return null;
+                    return expl;
                 }
 
                 //Get inferred date
-                if(result[1] != "DATE")
-                {
-                    return GetInferredDate(emrPath, line);
-                }
-
-                return result[2];
+                return GetInferredDate(emrPath, line);
             }
             catch(Exception e)
             {
@@ -54,9 +67,22 @@ namespace emr_corefsol_service.Libs
             }
         }
 
+        private string GetExplicitDate(string line)
+        {
+            foreach(Regex r in _explicit)
+            {
+                var match = r.Match(line);
+                if (match.Success)
+                {
+                    return match.Value;
+                }
+            }
+            return null;
+        }
+
         private string GetInferredDate(string emrPath, string line)
         {
-            foreach(Regex r in _list)
+            foreach(Regex r in _inferred)
             {
                 var match = r.Match(line);
                 if (match.Success)
