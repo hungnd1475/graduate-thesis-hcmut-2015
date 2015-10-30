386446d0fd96afa0b66e0af6583a71c339c9807a
Tuấn Vương <vanhtuan0409@gmail.com>
2015-10-30 10:17:24 +0700

    add project extract world knowledge

---
+++ projects/emr-coreference-resolution/EMRCorefResol.ExtractWordKnowledge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HCMUT.EMRCorefResol.IO;
+
+namespace HCMUT.EMRCorefResol.ExtractWordKnowledge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
