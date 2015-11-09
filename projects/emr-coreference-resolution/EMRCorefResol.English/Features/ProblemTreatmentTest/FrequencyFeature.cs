f4f443ff946acf8a795c8711856985b1a9d45ace
Tuấn Vương <vanhtuan0409@gmail.com>
2015-11-09 23:33:06 +0700

    normalize frequency

--- projects/emr-coreference-resolution/EMRCorefResol.English/Features/ProblemTreatmentTest/FrequencyFeature.cs
+++ projects/emr-coreference-resolution/EMRCorefResol.English/Features/ProblemTreatmentTest/FrequencyFeature.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace HCMUT.EMRCorefResol.English.Features
@@ -13,12 +14,81 @@ namespace HCMUT.EMRCorefResol.English.Features
         {
             if (ana == null || ante == null) return;
 
-            if(ana.Frequency.Length > 0 &&
-                ante.Frequency.Length > 0 && 
-                string.Equals(ana.Frequency, ante.Frequency, StringComparison.InvariantCultureIgnoreCase))
+            if(!string.IsNullOrEmpty(ana.Frequency) &&
+                !string.IsNullOrEmpty(ante.Frequency))
             {
-                SetCategoricalValue(1);
+                var anaNorm = Normalized(ana.Frequency);
+                var anteNorm = Normalized(ante.Frequency);
+                if (anaNorm.Equals(anteNorm))
+                {
+                    SetCategoricalValue(1);
+                }
             }
+        }
+
+        private string Normalized(string freq)
+        {
+            var normalized = freq.Replace(".", "").Trim();
+
+            var daily = @"(daily|qod|^a day$|^once daily$|q[ ]{0,}(day|d)|every(.*?)day|q[ ]{0,}24[ ]{0,}(hours|hour|hrs|h)?|(once |1 )?(per|a) day)";
+            if (Regex.IsMatch(normalized, daily, RegexOptions.IgnoreCase))
+            {
+                return "daily";
+            }
+
+            var bid = @"(bid|twice daily|q[ ]{0,}12[ ]{0,}(hours|hour|hrs|h)?|(twice|2 time(s)?|2) (a|per) day|times two)";
+            if (Regex.IsMatch(normalized, bid, RegexOptions.IgnoreCase))
+            {
+                return "bid";
+            }
+
+            var tid = @"(tid|^3[ ]{0,}x[ ]{0,}(a )?day$|q[ ]{0,}8[ ]{0,}(hours|hour|hrs|h)?|(three|3) time(s)? (a|per) day|every 8 hour(s)?|times three)";
+            if (Regex.IsMatch(normalized, tid, RegexOptions.IgnoreCase))
+            {
+                return "tid";
+            }
+
+            var qid = @"(qid|q[ ]{0,}6[ ]{0,}(hours|hour|hrs|h)?|(four|4) time(s)? (a|per) day|times four)";
+            if (Regex.IsMatch(normalized, qid, RegexOptions.IgnoreCase))
+            {
+                return "qid";
+            }
+
+            var qam = @"(qam|every morning|in (the )?morning)";
+            if (Regex.IsMatch(normalized, qam, RegexOptions.IgnoreCase))
+            {
+                return "qam";
+            }
+
+            var qpm = @"(qpm|every morning|in (the )?morning)";
+            if (Regex.IsMatch(normalized, qpm, RegexOptions.IgnoreCase))
+            {
+                return "qpm";
+            }
+
+            var qhs = @"(qhs|at betime)";
+            if (Regex.IsMatch(normalized, qhs, RegexOptions.IgnoreCase))
+            {
+                return "qhs";
+            }
+
+            var qtimes = @"q[ ]{0,}(\d+)[ ]{0,}(hours|hour|hrs|h)?|every (\d+) hour(s)?";
+            var match = Regex.Match(normalized, qtimes, RegexOptions.IgnoreCase);
+            if (match.Success)
+            {
+                var number = match.Groups[1];
+                return $"q{number}h";
+            }
+
+            var every = @"every (\d+) (hours|hour|hrs|h)";
+            match = Regex.Match(normalized, qtimes, RegexOptions.IgnoreCase);
+            if (match.Success)
+            {
+                var number = match.Groups[1];
+                return $"q{number}h";
+            }
+
+            return normalized.Trim().ToLower();
         }
     }
 }
